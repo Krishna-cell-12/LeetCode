3cdffdddc1069e9284b1d80372f7409ab2569958
Krishna Abhang <krishnaabhang2022@gmail.com>
2026-08-03 22:06:00 +0530

    Time: 38 ms (39.20%) | Memory: 91.6 MB (47.73%) - LeetSync

---
+++ 1522-stone-game-iii/stone-game-iii.java
@@ -0,0 +1,37 @@
+class Solution {
+    int n;
+    int dp[];
+    
+    public String stoneGameIII(int[] s) {
+        n = s.length;
+        dp = new int[n];
+        Arrays.fill(dp, Integer.MIN_VALUE);
+
+        int diff = solve(s, 0);
+
+        if (diff > 0) {
+            return "Alice";    
+        } else if (diff < 0) {
+            return "Bob";
+        } else {
+            return "Tie";
+        }
+    }
+
+    public int solve(int[] s, int i) {
+        if (i >= n) {
+            return 0;
+        }
+        if (dp[i] != Integer.MIN_VALUE) {
+            return dp[i];
+        }
+        
+        int sum = 0;
+        int maxDiff = Integer.MIN_VALUE;
+        for (int k = 0; k < 3 && i + k < n; k++) {
+            sum += s[i + k];
+            maxDiff = Math.max(maxDiff, sum - solve(s, i + k + 1));
+        }
+        return dp[i] = maxDiff;
+    }
+}
